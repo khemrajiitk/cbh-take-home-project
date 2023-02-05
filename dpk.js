fe2e33a4eb1ea8cfa93edf2037827cdb963ea4af
khemrajmeena0111 <khemrajmeena0111@gmail.com>
2023-02-06 00:32:13 +0530

    feat: Refactoring completed

--- dpk.js
+++ dpk.js
@@ -3,26 +3,27 @@ const crypto = require("crypto");
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
+
+  //return if there is invalid input
+  if (!event) {
+    return TRIVIAL_PARTITION_KEY;
+  }
+
   let candidate;
-
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
-  }
-
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
+  if (event && event.partitionKey) {
+    candidate = `${event.partitionKey}`;
   } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+    const data = JSON.stringify(event);
+    candidate = crypto.createHash("sha3-512").update(data).digest("hex");
   }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+
+  if (candidate.length <= MAX_PARTITION_KEY_LENGTH) {
+    return candidate;
   }
-  return candidate;
+
+  return crypto.createHash("sha3-512").update(candidate).digest("hex");
 };
+
+//In previously given code there are nested if else conditions and we are checking for invalid input
+// in the middle of function. I checked validation initially and if it is invalid simply return default output
+// In my implementation sequence on execution is very close to a business requirement so it is more readable
